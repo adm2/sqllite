928081cb3e8d6d81c5b50da1185eb6e19f69a91a
Sergey Pashkov <pashkov.s.y@gmail.com>
2019-05-19 13:37:03 +0500

    update

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-pass
+import pprint
+import sys
+import sqlite3 as lite
+
+data = ''
+
+con = None
+
+query_string = '''
+    SELECT Lastname
+    FROM Employee
+    ORDER BY Lastname
+    LIMIT 10
+'''
+
+try:
+    con = lite.connect('Chinook_Sqlite.sqlite')
+    cur = con.cursor()
+    cur.execute(query_string)
+    con.commit()
+    con.rollback()
+    print('=================================')
+    pprint.pprint(cur.fetchall())
+    print('=================================')
+    pprint.pprint(data)
+
+except Exception as e:
+    print(e)
+    sys.exit(1)
+finally:
+    if con is not None:
+        con.close()
