2bf0285210c89d926f2fa75a7d6d99ec0712e6c0
Sergey Pashkov <pashkov.s.y@gmail.com>
2019-05-19 19:02:33 +0500

    final

--- task1.py
+++ task1.py
@@ -13,12 +13,9 @@ query_string = '''
     JOIN Invoice ON Customer.CustomerID = Invoice.CustomerID
     JOIN InvoiceLine ON InvoiceLine.InvoiceID = Invoice.InvoiceID
     JOIN Track ON InvoiceLine.TrackID = Track.TrackID
+    JOIN Genre ON Track.GenreID = Genre.GenreID
 
-
-
-
-
-    WHERE Employee.BirthDate < "1969-05-19"
+    WHERE Genre.Name NOT "Rock" AND Employee.BirthDate < "1969-05-19"
     ORDER BY Employee.City ASC, Employee.Email DESC
     LIMIT 10
 
