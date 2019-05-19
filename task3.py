0836b6c0321e9f6fd55b4e227b73fbe1bb7bc5b2
Sergey Pashkov <pashkov.s.y@gmail.com>
2019-05-19 17:48:33 +0500

    final

--- task3.py
+++ task3.py
@@ -9,10 +9,8 @@ con = None
 query_string = '''
     SELECT FirstName, Phone
     FROM Customer 
-    LEFT JOIN Invoice 
-        ON Customer.CustomerID = Invoice.CustomerID   
-    LEFT JOIN InvoiceLine 
-        ON InvoiceLine.InvoiceID = Invoice.InvoiceID    
+    LEFT JOIN Invoice ON Customer.CustomerID = Invoice.CustomerID   
+    LEFT JOIN InvoiceLine ON InvoiceLine.InvoiceID = Invoice.InvoiceID    
     WHERE InvoiceLine.UnitPrice = (SELECT max(InvoiceLine.UnitPrice) FROM InvoiceLine)
     ORDER BY FirstName
 '''
